d757431c633e98348d89248b326865f34b9cae1f
PRLL <josemoises.toyosvargas@hotmail.com>
2022-03-20 11:36:16 -0400

    upd: settings

--- djangotut/settings.py
+++ djangotut/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^#67v_qk7zem8tgyd41u(5xm*q3n+1!$1845p2lw-)0u%v-lr&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'DO'
 
 USE_I18N = True
 
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
