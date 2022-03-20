ae983fca4ccbd2450c670a7870dc473094c76bfa
PRLL <josemoises.toyosvargas@hotmail.com>
2022-03-20 15:27:41 -0400

    add: detail page for blogs

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
 from django.utils import timezone
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    return render(request, 'blog/post_list.html', {'posts': Post.objects.filter(published_date__lte = timezone.now()).order_by('published_date')})
+
+def post_detail(request, pk):
+    return render(request, 'blog/post_detail.html', {'post': get_object_or_404(Post, pk=pk)})
